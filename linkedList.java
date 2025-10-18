c283e12f7ce647c243c179102410ec6f8deb7397
jamiuq <jamiuq@farmingdale.edu>
2025-10-17 21:21:59 -0400

    Create linkedList.java

---
+++ linkedList.java
@@ -0,0 +1,24 @@
+
+
+public class linkedList
+{
+  private int data;
+  private linkedList next;
+
+  public linkedList(int data)
+  {
+    this.data = data;
+  }
+
+  public linkedList(int data, linkedList next)
+  {
+    this.data = data;
+    this.next = next;
+  }
+
+  public int getData() {return data;}
+  public void setData(int data) {this.data = data;}
+
+  public linkedList getNext() {return next;}
+  public void setNext(linkedList next) {this.next = next;}
+}
